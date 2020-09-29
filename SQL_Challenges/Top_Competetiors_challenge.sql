f97f67f685d5ce5de6b0152d1c61230457b7c89a
Riyaz Ali <54612827+aayrm5@users.noreply.github.com>
2020-09-29 20:03:04 +0530

    Adding Top Competitors challenge sol

---
+++ SQL_Challenges/Top_Competetiors_challenge.sql
@@ -0,0 +1,16 @@
+SELECT 
+    h.hacker_id, h.name
+FROM
+    Submissions s
+        JOIN
+    Challenges c ON s.challenge_id = c.challenge_id
+        JOIN
+    Difficulty d ON c.difficulty_level = d.difficulty_level
+        JOIN
+    Hackers h ON h.hacker_id = s.hacker_id
+WHERE
+    s.score = d.score
+GROUP BY h.hacker_id , h.name
+HAVING COUNT(s.hacker_id) > 1
+ORDER BY COUNT(s.hacker_id) DESC , hacker_id;
+
