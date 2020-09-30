6e51f9e088d78e7de651ff7eb20432c2b6a2b068
Riyaz Ali <54612827+aayrm5@users.noreply.github.com>
2020-09-30 18:59:34 +0530

    Calculating Weighted Mean through various ways; viz., Numpy, List Comprehensions and Arithmatic Operations.

--- 10 Days of Statistics/Day0_Weighted_Mean.py
+++ 10 Days of Statistics/Day0_Weighted_Mean.py
@@ -1,3 +1,6 @@
+#Calculating the weighted mean of X with respect to W; 
+# N is the number of observations;
+
 N = int(input().strip())
 
 X = list(map(int,input().strip().split()))
@@ -8,6 +11,10 @@ for i in range(N):
     NUM = NUM + (X[i]*W[i])
 
 print("{0:.1f}".format(NUM/sum(W)))
+
+#list comprehensions method
+print(round(sum([X[i]*W[i] for i in range(N)])/sum(W),1))
+
 #______________________________________
 #Using Numpy
 import numpy as np
