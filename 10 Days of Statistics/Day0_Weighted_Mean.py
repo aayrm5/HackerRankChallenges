3f969e1ed6ff9bcfbcce1506797c023a64ded0a7
Riyaz Ali <54612827+aayrm5@users.noreply.github.com>
2020-09-30 17:34:18 +0530

    Caclulating Weighted Mean Challenge solution.

---
+++ 10 Days of Statistics/Day0_Weighted_Mean.py
@@ -0,0 +1,10 @@
+N = int(input().strip())
+
+X = list(map(int,input().strip().split()))
+W = list(map(int,input().strip().split()))
+
+NUM = 0
+for i in range(N):
+    NUM = NUM + (X[i]*W[i])
+
+print("{0:.1f}".format(NUM/sum(W)))
